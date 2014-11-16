236ecfebe03b1172904559409d1761f3326e8b80
dsbrown <david.scott.brown@gmail.com>
2014-11-16 08:21:34 -0800

    Adding test files
    These are CSV files used for testing, the data is meaningless

--- csvparse/csvparse.py
+++ csvparse/csvparse.py
@@ -3,26 +3,28 @@
 
 ###################################################################################
 #
-# 							Basic Shell Program
+# 							CSV Parsing Program
 #
 # Description:
-# This is my basic command line based program skeleton for creating other programs
+# Open and parse csv files
 #
 # Author: David S. Brown
-# Last Major Change: 14 November 2014
+# Last Major Change: 15 November 2014
 #
 ####################################################################################
 
 import argparse
 import sys
-parser = argparse.ArgumentParser(description='A skeletal framework for writing command line based programs',
-	epilog="And that's how you'd foo a bar",
-	prefix_chars='-+',
+import csv
+
+parser = argparse.ArgumentParser(description='Parse CSV files',
+	#epilog="to be used only for good",
+	#prefix_chars='-+',
 	)
 
 ####################################################################################
 #
-# 							Debugging & Verbosity Flags
+# 									 Flags
 #
 ####################################################################################
 
@@ -30,98 +32,98 @@ parser = argparse.ArgumentParser(description='A skeletal framework for writing c
 parser.add_argument("-v", "--verbose", action="count", default=0, help="increase output verbosity")
 # a numeric debug level as in arg_parse.py -D 3
 parser.add_argument("-D", "--debug", type=int, default=0, help="Set integer debug level from 0-9 as: -D 3")
-# a logical flag true or false as in arg_parse.py -q
-parser.add_argument("-q", "--quiet", action="store_false", dest="verbose", default=True, help="Please be quiet, no output to stdout")
-
-
-
-####################################################################################
-#
-# 									Filename Flags
-#
-####################################################################################
-# MULTIPLE FILENAME METHOD 1 - use this to call files with -f example: arg_parse.py  -f foo.txt bar.txt fu.txt
-#parser.add_argument("-f", "--files", nargs="+", dest="mfiles", help="file names to processed seperated by spaces")
-
-# MULTIPLE FILENAME METHOD 2 - use this to call files on the end of the command line example: arg_parse.py foo.txt bar.txt fu.txt
-#parser.add_argument(nargs="+", dest="mfiles", help="file names to processed seperated by spaces")
-
-# MULTIPLE FILENAME METHOD 3 - use this to call files on the end of the command line example: arg_parse.py  -f foo.txt -f bar.txt -f fu.txt 
-#parser.add_argument('-f', '--file', dest="mfiles", action='append', help="file names to processed in this format: -f file1 -f file2 -f file3")
-
-# SINGLE FILENAME METHOD 1 - use this to call a file on the end of the command line example: ./arg_parse.py -vv foo.txt 
-#parser.add_argument('inputfile', help="input file name to work on")
-
-# SINGLE FILENAME METHOD 2 - use this to call a file on the end of the command line example: ./arg_parse.py -vv -f foo.txt 
-#parser.add_argument('inputfile', help="input file name to work on")
-
-# MULTIPLE FILENAME METHOD 4 - Allow Optional Input and output files using positional arguments
-parser.add_argument('infile',  nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-
-
-####################################################################################
-#
-# 									Other Flags
-#
-####################################################################################
-#parser.add_argument("square", type=int, help="display a square of a given number")
-
-#Here is a tri-mode flag - my terminology , see parser call above
-#	./arg_parse.py  
-#	Last =-1
-#	./arg_parse.py -L 
-#	Last =False
-#	./arg_parse.py +L 
-#	Last =True
-parser.add_argument('-L', '-Last', action="store_false", default=-1, dest="Last", help="Do not assert Last")
-parser.add_argument('+L', '+Last', action="store_true",  dest="Last", help="Assert Last")
-
-# Some practical flags
-parser.add_argument('-d', '-delimiter', default=",", help="Optionally specify the field separation delimiter used, the default is ','")
-parser.add_argument('-i', '-interactive', action="store_true", default=False, 
+parser.add_argument('-i', '-interactive', dest="interactive", action="store_true", default=False, 
 	help="This will output helpful information about the file its processing and ask if you want to continue, the default is non-interactive",
 	)
 
+# Files to open, one by one as in csvparse.py -f foo.txt bar.txt fu.txt 
+parser.add_argument("-f", "--files", nargs="+", dest="mfiles", help="file names to open separated by spaces")
+
+# You may optionally change the delimeter and quote character
+parser.add_argument('-d', '-delimiter', default=",", help="Optionally specify the field separation delimiter used, the default is ','")
+parser.add_argument('-q', '-quote_delimiter', default='"', help="Optionally specify the quote delimiter that is being used, the default is \"")
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+    
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes":"yes",   "y":"yes",  "ye":"yes",
+             "no":"no",     "n":"no"}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while 1:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return default
+        elif choice in valid.keys():
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "\
+                             "(or 'y' or 'n').\n")
+
+
+
 ####################################################################################
 #
-# 									Demonstration Code
+# 									Main
 #
 ####################################################################################
 
 args = parser.parse_args()
 
+if args.debug >= 2: print("args: ", args)
 
-# Demonstration of numeric debug and verbose level also integer value
 #
-# print("Debug level: {}".format(args.debug))
+# Open files and if interactive examine the file to see if the header is present
+#
+filesProcessed = []
+filesSkipped = []
+for mfile in args.mfiles:
+	if (args.debug >= 1) or (args.verbose >=1):
+		print("filename:{}".format(mfile))
+	f = open(mfile, 'rU')
+	line = f.readline()
+	#line=line.strip()
+	#line=line.split(",")
+	line=line.strip().split(",")
+	#s="First line of the file '" + str(mfile) + "' has " + str(len(line)) + " columns which are:"
+	#print(s)
 
-# answer = args.square**2
-# if args.debug >= 2:
-# 	print("args: ", args)
-# elif args.debug >= 1:
-# 	print("n={}".format(args.square, args.square))
-# else:
-# 	True
+	if args.interactive >= 1:
+		print("First line of the file '" + str(mfile) + "' has " + str(len(line)) + " columns which are:")
+		print("{}".format(line))
+		answer=query_yes_no("Is this the csv file header?",default="yes")
+		if args.debug >= 2: print("{}".format(answer))
+		if answer is "no":
+			print("skipping")
+			filesSkipped.append(mfile)
+			continue
 
-# if args.verbose >= 2:
-#    print("{}^2 == {}".format(args.square, answer))
-# elif args.verbose >= 1:
-#    print(answer)
-# else:
-#    print("the square of {} equals {}".format(args.square, answer))
+	#
+	#Open CSV file
+	f.seek(0)
+	csv_f = csv.reader(f)
+	for row in csv_f:
+	  print row[2]
 
+	filesProcessed.append(mfile)
 
-# Demonstration Mutli file name method 1,2,3
-#print(args.mfiles)
-
-#Single file name demonstration method 1 & 2
-#print(args.inputfile)
-#for mfile in args.mfiles
-#	print("filename:{}".format(mfile))
-
-# Demonstration of stdin/stdout - Multi file name method 4
-# call as ./arg_parse.py foo.txt bar.txt
-print(args.infile, args.outfile)
-print("Last ={}".format(args.Last))
+if args.interactive >= 1:
+	print("Files procesed: " + str(filesProcessed))
+	print("Files skipped: " + str(filesSkipped))
 
