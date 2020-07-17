2107fc54df9039955cd148978f6e65f73ff56c3c
David S. Brown <dsbrown@users.noreply.github.com>
2020-07-16 19:36:13 -0700

    more updates of the python scraps directory

--- csv2sql/test_get_hash.py
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/python
-# -*- coding: utf-8 -*-
-
-d={'whale':'blue','ray':'manta','beetle':'lady bug','dog':'cocker'}
-search_fields = ('whale','frog','ray','beetle','bug')
-for i in search_fields:
-    if d.get(i):
-        print "found "+d[i]+" using key "+i
